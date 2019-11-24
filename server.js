3ad8ed53010357b8d5fcfa3cf1dc1ad69f530eee
Ubuntu <ubuntu@ip-172-31-19-233.us-east-2.compute.internal>
2019-11-24 12:25:59 +0000

    minor prd fix

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const cors = require("cors");
 
 const app = express();
 
-const allowedOrigins = ["http://localhost:3000"];
+// const allowedOrigins = ["https://master.dby3j7novssz9.amplifyapp.com", "http://localhost:3000"];
 app.use(cors({
     origin: function(origin, callback){
         if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1){
-            const msg = "The CORS policy for this site does not " +
-                "allow access from the specified Origin.";
-            return callback(new Error(msg), false);
-        }
+       // if(allowedOrigins.indexOf(origin) === -1){
+         //   const msg = "The CORS policy for this site does not " +
+           //     "allow access from the specified Origin.";
+          //  return callback(new Error(msg), false);
+      //  }
         return callback(null, true);
     },
     credentials: false,
